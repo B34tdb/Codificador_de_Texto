c97e2797e2c24f930686a5f4a557013abfac6769
Hyan Araujo <hyan.araujoalmeida@gmail.com>
2021-09-01 00:14:28 -0300

    secundo commit

---
+++ cifra.js
@@ -0,0 +1,6 @@
+const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const codificar = document.getElementById('codificar');
+const codificar2 = document.getElementById('codificar2');
+const cavalcanti = document.getElementById('ola');
+const radio = document.getElementById ("radio");
+const  radio = document.getElementById("radio2")
