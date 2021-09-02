a3d61b680e936546cba07bc44714077944f15ac8
Hyan Araujo <hyan.araujoalmeida@gmail.com>
2021-09-02 00:34:15 -0300

    terceiro commit

---
+++ base64.js
@@ -0,0 +1,11 @@
+function bas(){
+    let cod1 = document.getElementById("codificar").value;
+    let cod2 = document.getElementById("codificar2");
+    let escolha = document.getElementById("radio1").checked
+    cod2.value = base64Logic(cod1, escolha) 
+}
+
+function base64Logic(cod1,escolha){
+    return (escolha)? btoa(cod1) : atob(cod1)
+}
+        
--- cifra.js
+++ cifra.js
@@ -1,6 +1,37 @@
-const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const codificar = document.getElementById('codificar');
-const codificar2 = document.getElementById('codificar2');
-const cavalcanti = document.getElementById('ola');
-const radio = document.getElementById ("radio");
-const  radio = document.getElementById("radio2")
+
+
+function cesarEntrada(arr, key){
+    return arr.map((c)=>{
+        let code = c.charCodeAt();
+        if(code >= 65 && code <= 90){
+            return String.fromCharCode(((code - 65 + key) % 26) + 65)
+        } else if(code >= 97 && code <= 122){
+            return String.fromCharCode(((code - 97 + key) % 26) + 97)
+        } else return c
+    }).join('')
+}
+
+function cesarSaida(arr, key){
+    return arr.map((c)=>{
+        let code = c.charCodeAt();
+        if(code >= 65 && code <= 90){
+            return (code-65-key < 0)?String.fromCharCode(((code - 65 - key + 26)%26)+65):String.fromCharCode(((code - 65 - key)%26)+65) 
+        } else if(code >= 97 && code <= 122){
+            return String.fromCharCode(((code - 97 - key + 26) % 26) + 97)
+        } else return c
+    }).join('')
+}    
+
+
+
+    function nar(){
+    let cod1 = document.getElementById("codificar").value.split("");
+    let cod2 = document.getElementById("codificar2");
+    let numbs = parseInt(document.getElementById("numbs").value)
+    let escolha = document.getElementById("radio1").checked
+
+    if(escolha){
+        cod2.value = cesarEntrada(cod1,numbs)
+    }else{
+        cod2.value = cesarSaida(cod1,numbs)
+    }} 
